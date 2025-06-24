c55728b70d3374ae0611f0296a9a50935c95561b
Absonumber1 <abdelrahmanadss2023@gmail.com>
2025-06-24 07:19:16 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.85a6f0fae26140368df667da3b62c8d0',
+  appName: 'creator-battle-points-showdown',
+  webDir: 'dist',
+  server: {
+    url: 'https://85a6f0fa-e261-4036-8df6-67da3b62c8d0.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  },
+  bundledWebRuntime: false
+};
+
+export default config;
